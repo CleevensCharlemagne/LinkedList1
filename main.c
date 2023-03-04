e116594d27a435b4fa3aff0dc40cdb1cb40a1752
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-03 22:19:07 -0500

    update

--- main.c
+++ main.c
@@ -41,3 +41,15 @@ void insertFirst(int key, int data) {
    head = link;
 }
 
+//delete first item
+struct node* deleteFirst() {
+
+   //save reference to first link
+   struct node *tempLink = head;
+
+   //mark next to first link as first
+   head = head->next;
+
+   //return the deleted link
+   return tempLink;
+}
