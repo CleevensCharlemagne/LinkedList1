96893592324d89da38a107e93a9717bd547f5a9e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-03 22:18:52 -0500

    update

--- main.c
+++ main.c
@@ -25,3 +25,19 @@ void printList() {
 
    printf(" ]");
 }
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+
+   link->key = key;
+   link->data = data;
+
+   //point it to old first node
+   link->next = head;
+
+   //point first to new first node
+   head = link;
+}
+
