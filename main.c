6dd7513d229e8c62b5f420b88fc37de0b3d13ba5
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-03 22:23:45 -0500

    update

--- main.c
+++ main.c
@@ -134,3 +134,35 @@ struct node* delete(int key) {
 
    return current;
 }
+
+void sort() {
+
+   int i, j, k, tempKey, tempData;
+   struct node *current;
+   struct node *next;
+
+   int size = length();
+   k = size ;
+
+   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
+      current = head;
+      next = head->next;
+
+      for ( j = 1 ; j < k ; j++ ) {
+
+         if ( current->data > next->data ) {
+            tempData = current->data;
+            current->data = next->data;
+            next->data = tempData;
+
+            tempKey = current->key;
+            current->key = next->key;
+            next->key = tempKey;
+         }
+
+         current = current->next;
+         next = next->next;
+      }
+   }
+}
+
