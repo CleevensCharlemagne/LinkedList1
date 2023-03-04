31668c8e15ce35e5b4151237228d669086877cc3
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-03 22:24:03 -0500

    update

--- main.c
+++ main.c
@@ -166,3 +166,17 @@ void sort() {
    }
 }
 
+void reverse(struct node** head_ref) {
+   struct node* prev   = NULL;
+   struct node* current = *head_ref;
+   struct node* next;
+
+   while (current != NULL) {
+      next  = current->next;
+      current->next = prev;
+      prev = current;
+      current = next;
+   }
+
+   *head_ref = prev;
+}
